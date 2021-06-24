d4d91e6f256fd7bbfec627a64bf8ea3565798080
ClaudenyAvelino <claudeny.avelino78@academico.ifs.edu.br>
2021-06-23 22:55:54 -0300

    Segundo Projeto

--- gameplay/App.tsx
+++ gameplay/App.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
 import { StatusBar } from 'react-native';
-import AppLoading from 'expo-app-loading';
-import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
-import { SignIn } from './src/screens/SignIn';
+//import { Routes } from './src/routes';
+import { Home } from './src/screens/Home';
+import { Background } from './src/components/Background';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,13 +23,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <SignIn />
-    </>
+      <Home />
+    </Background>
   );
 }
